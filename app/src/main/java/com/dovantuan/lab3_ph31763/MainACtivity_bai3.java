6b26ce285c86f4fef246e8e99acd8ad857c51b30
Đỗ Văn Tuấn <129986145+tuando18@users.noreply.github.com>
2023-05-19 16:20:24 +0700

    Lab 3 - Đỗ Văn Tuấn

---
+++ app/src/main/java/com/dovantuan/lab3_ph31763/MainACtivity_bai3.java
@@ -0,0 +1,14 @@
+package com.dovantuan.lab3_ph31763;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainACtivity_bai3 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_bai3);
+    }
+}
